c650838177c06e3fb322f873a3298c074c366255
JJ Merelo <jjmerelo@gmail.com>
2016-09-26 17:34:21 +0200

    Compila de nuevo

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,9 +7,20 @@ set :gopath, deploy_to
 set :pid_file, deploy_to+'/pids/PIDFILE'
 set :symlinks, { "pids" => "pids" }
 
+
+task :production do
+  server "jjmerelo-8639.nitrouspro.com", user: "nitrous", port: 32769
+end
+
+after 'deploy:updated', 'go:build'
+
 namespace :go do
-  task :production do
-    server "jjmerelo-8639.nitrouspro.com", user: "nitrous", port: 32769
+  task :build do
+    with_env('GOPATH', gopath) do
+      run "go get -u github.com/JJ/CuantoQuedaBot"
+      run "mkdir #{release_path}/bin"
+      run "cp /home/#{:user}/go/bin/CuantoQuedaBot #{release_path}/bin/"
+    end
   end
 end
 
