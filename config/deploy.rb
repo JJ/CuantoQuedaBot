6a828f767e4be5b3fe60b349e5d7aecbce752a68
JJ Merelo <jjmerelo@gmail.com>
2016-09-26 18:04:09 +0200

    Error happens somewhere else
    With help from http://stackoverflow.com/questions/20410138/how-do-i-configure-remote-refs-for-git-capistrano-3-in-ror-deployment/20411757#20411757

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "CuantoQuedaBot"
-set :repository, "https://github.com/JJ/CuantoQuedaBot.git"
+set :repo_url, "https://github.com/JJ/CuantoQuedaBot.git"
 
 set :user, 'nitrous'
 set :deploy_to, "/home/#{:user}/app"
@@ -7,6 +7,7 @@ set :gopath, deploy_to
 set :pid_file, deploy_to+'/pids/PIDFILE'
 set :symlinks, { "pids" => "pids" }
 
+role :app, "jjmerelo-8639.nitrouspro.com"
 
 task :production do
   server "jjmerelo-8639.nitrouspro.com", user: "nitrous", port: 32769
@@ -14,12 +15,14 @@ end
 
 after 'deploy:updated', 'go:build'
 
+set :default_env, { GOPATH: "/home/nitrous/lib/go" }
+
 namespace :go do
   task :build do
-    with_env('GOPATH', gopath) do
-      run "go get -u github.com/JJ/CuantoQuedaBot"
-      run "mkdir #{release_path}/bin"
-      run "cp /home/#{:user}/go/bin/CuantoQuedaBot #{release_path}/bin/"
+    on roles(:app) do 
+      execute "go get -u github.com/JJ/CuantoQuedaBot"
+      execute "mkdir #{release_path}/bin"
+      execute "cp /home/#{:user}/go/bin/CuantoQuedaBot #{release_path}/bin/"
     end
   end
 end
