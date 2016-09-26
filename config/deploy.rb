ae2c2b43b8b20516d8bcb1ab2ee0e2ec2d60af7c
JJ Merelo <jjmerelo@gmail.com>
2016-09-26 20:21:05 +0200

    Ahora parece que va

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,9 @@
 set :application, "CuantoQuedaBot"
 set :repo_url, "https://github.com/JJ/CuantoQuedaBot.git"
-
+token = ENV['BOT_TOKEN']
+puts "token is #{token}"
 user = "nitrous"
+
 set :user, user
 set :deploy_to, "/home/#{user}/app"
 set :gopath, deploy_to
@@ -19,9 +21,9 @@ after 'deploy:updated', 'go:build'
 namespace :go do
   task :build do
     on roles(:app) do
-      execute "export GOROOT=/usr/local/opt/go;export GOPATH=/home/nitrous/lib/go;cd #{release_path};/usr/local/opt/go/bin/go get;/usr/local/opt/go/bin/go tool compile *.go"
-      execute "mkdir #{release_path}/bin"
-      execute "cp /home/#{user}/go/bin/CuantoQuedaBot #{release_path}/bin/"
+      execute "export GOROOT=/usr/local/opt/go;export GOPATH=/home/nitrous/lib/Go;export GOBIN=$GOPATH/bin;cd #{release_path};/usr/local/opt/go/bin/go get"
+      execute "export GOROOT=/usr/local/opt/go;export GOPATH=/home/nitrous/lib/Go;export GOBIN=$GOPATH/bin;cd #{release_path};export BOT_TOKEN=#{token};/usr/local/opt/go/bin/go run CuantoQuedaBot.go"
+
     end
   end
 end
