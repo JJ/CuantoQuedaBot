d1ed6b4b00687e135b8223cb287bee705009f660
JJ Merelo <jjmerelo@gmail.com>
2016-09-28 17:50:58 +0200

    Funcionando fabric

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-from fabric.api import local
+from fabric.api import env, run
+
+env.hosts = [ '159.100.248.62' ]
+env.user = "root"
 
 def uptime():
-  local('uptime')
+  run('uptime')
