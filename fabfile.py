8265cd7a701c0c06c675a5da2d37391f21cdd17c
JJ Merelo <jjmerelo@gmail.com>
2016-10-01 08:58:20 +0200

    Avanza #2 con Fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,11 @@ from fabric.api import env, run
 
 env.hosts = [ '159.100.248.62' ]
 env.user = "root"
+env.release_path= "CuantoQuedaBot"
 
 def uptime():
-  run('uptime')
+    run('uptime')
+  
+  
+def build(goroot="/usr", gopath="/home/%s/lib/Go" % env.user):
+    run("export GOROOT=%(goroot)s;export GOPATH=%(gopath)s;export GOBIN=$GOPATH/bin;cd %(release_path)s;$GOBIN/go get" % { 'goroot': goroot, 'gopath': gopath, 'release_path':env.release_path} )
