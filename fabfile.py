fafad48ae924da3c5d814fdd7fb44fa51f2d646c
JJ Merelo <jjmerelo@gmail.com>
2016-10-01 09:03:22 +0200

    Avanza #2 con Fabfile

--- fabfile.py
+++ fabfile.py
@@ -10,4 +10,4 @@ def uptime():
   
   
 def build(goroot="/usr", gopath="/home/%s/lib/Go" % env.user):
-    run("export GOROOT=%(goroot)s;export GOPATH=%(gopath)s;export GOBIN=$GOPATH/bin;cd %(release_path)s;$GOBIN/go get" % { 'goroot': goroot, 'gopath': gopath, 'release_path':env.release_path} )
+    run("export GOROOT=%(goroot)s;export GOPATH=%(gopath)s;export GOBIN=$GOPATH/bin;cd %(release_path)s;echo $GOBIN; echo $GOPATH; $GOBIN/go get" % { 'goroot': goroot, 'gopath': gopath, 'release_path':env.release_path} )
