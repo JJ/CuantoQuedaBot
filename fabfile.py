ac8542a63b6811dffdd9356b846bb5ffc4e647ca
JJ Merelo <jjmerelo@gmail.com>
2016-10-01 09:08:17 +0200

    Quita cosas de enmedio

--- fabfile.py
+++ fabfile.py
@@ -9,5 +9,5 @@ def uptime():
     run('uptime')
   
   
-def build(goroot="/usr", gopath="/home/%s/lib/Go" % env.user):
-    run("export GOROOT=%(goroot)s;export GOPATH=%(gopath)s;export GOBIN=$GOPATH/bin;cd %(release_path)s;echo $GOBIN; echo $GOPATH; $GOBIN/go get" % { 'goroot': goroot, 'gopath': gopath, 'release_path':env.release_path} )
+def build(goroot="/usr", gopath="/home/%s/lib/go" % env.user):
+    run("export GOROOT=%(goroot)s;export GOPATH=%(gopath)s;export GOBIN=$GOPATH/bin;cd %(release_path)s;echo $GOBIN; echo $GOPATH; $GOROOT/bin/go get" % { 'goroot': goroot, 'gopath': gopath, 'release_path':env.release_path} )
